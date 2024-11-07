1302c21c833d1d0e92b20874b5cb9a3583aba716
Yuxin <55794321+yvonneyx@users.noreply.github.com>
2024-11-07 12:45:52 +0800

    feat: set graph default height (#559)
    * feat: set graph default height
    
    * chore: deps

--- packages/graphin/src/hooks/useGraph.ts
+++ packages/graphin/src/hooks/useGraph.ts
@@ -1,5 +1,5 @@
 import { Graph } from '@antv/g6';
-import { useEffect, useRef, useState } from 'react';
+import { CSSProperties, useEffect, useRef, useState } from 'react';
 import type { GraphinProps } from '../types';
 
 /**
@@ -12,6 +12,7 @@ export default function useGraph<P extends GraphinProps>(props: P) {
   const [isReady, setIsReady] = useState(false);
   const graphRef: React.MutableRefObject<Graph | null> = useRef(null);
   const containerRef: React.MutableRefObject<HTMLDivElement | null> = useRef(null);
+  const [containerHeight, setContainerHeight] = useState<CSSProperties['height']>('inherit');
 
   useEffect(() => {
     if (graphRef.current || !containerRef.current) return;
@@ -40,12 +41,21 @@ export default function useGraph<P extends GraphinProps>(props: P) {
     if (!options || !container || !graph || graph.destroyed) return;
 
     graph.setOptions(options);
-    graph.render().then(() => onReady?.(graph));
+    graph
+      .render()
+      .then(() => onReady?.(graph))
+      .then(() => {
+        // 优先使用父容器高度，否则使用画布高度
+        // Use the parent container height by default, otherwise use the canvas height
+        const parentHeight = container.parentElement?.clientHeight;
+        setContainerHeight(parentHeight ? 'inherit' : graph.getCanvas().getConfig().height);
+      });
   }, [options]);
 
   return {
     graph: graphRef.current,
     containerRef,
+    containerHeight,
     isReady,
   };
 }
